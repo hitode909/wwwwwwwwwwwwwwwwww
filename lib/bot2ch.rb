b7e8569fdcad51ddfd99f73e5d01cfac0320ac56
hitode909 <hitode909@gmail.com>
2010-10-26 09:22:16 +0900

    boards

--- lib/bot2ch.rb
+++ lib/bot2ch.rb
@@ -17,6 +17,17 @@ module Bot2ch
       @bbsmenu = 'http://menu.2ch.net/bbsmenu.html'
     end
 
+    def boards
+      list = []
+      reg = Regexp.new("<a href=(http://\\w+\\.2ch\\.net/\\w+/)>", Regexp::IGNORECASE) # TODO: test
+      CachedResource.resource(@bbsmenu) do |f|
+        f.each_line{|line|
+          list << Board.new($1) if line.encode("utf-8", "sjis") =~ reg
+        }
+      end
+      list
+    end
+
     def get_board(subdir)
       reg = Regexp.new("href=(.+#{subdir})", Regexp::IGNORECASE)
       CachedResource.resource(@bbsmenu) do |f|
